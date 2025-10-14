50331306dd848a8083a163fd54c44412b20e0751
babubabitha924-eng <babubabitha924@gmail.com>
2025-10-14 19:59:40 +0530

    Update python17.py

--- python17.py
+++ python17.py
@@ -1,3 +1,3 @@
 lst = list(map(int, input("Enter numbers separated by space: ").split()))
-evens = [num for num in lst if num % 2 == 0]
-print(evens)
+odd = [num for num in lst if num % 2 != 0]
+print(odd)
